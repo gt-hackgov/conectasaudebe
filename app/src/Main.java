72e246b84d126278f9da011422879c551ef7dc00
gabcrvlh <gabrieladcrvlh@gmail.com>
2026-06-07 20:21:23 -0300

    [Main] add menu para cadastro

--- app/src/Main.java
+++ app/src/Main.java
@@ -1,45 +1,53 @@
 import model.Paciente;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println(":::::Iniciando sistema:::::");
 
-        //Criando um paciente (objeto do tipo Paciente)
-        Paciente paciente1 = new Paciente("Ava Daniels");
+        Paciente paciente = new Paciente();
 
-        //Atribuindo valores ao objeto paciente1
-        paciente1.id = UUID.randomUUID();
-        paciente1.cpf = "111.111.111-11";
-        paciente1.nomeCompleto = "Ava Daniels";
-        paciente1.dataNascimento = LocalDate.of(1990, 01, 01);
-        paciente1.endereco = "Rua 1";
-        paciente1.email = "email@email.com";
-        paciente1.telefone = "(11)91111-1111";
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
 
-        //Exibindo valores dos atributos do objeto paciente1
-        System.out.println("Dados do paciente 1:" +
-                "\nNome: " + paciente1.nomeCompleto +
-                "\nID: " + paciente1.id +
-                "\nEmail: " + paciente1.email
-        );
+        do {
+            System.out.println("\nMenu: \n1. Cadastrar Paciente \n2. Exibir Paciente \n3. Sair");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o nome do paciente:");
+                    String nome = scanner.next() + scanner.nextLine();
+
+                    System.out.println("Digite o email:");
+                    String email = scanner.next() + scanner.nextLine();
+
+                    System.out.println("Digite o cpf:");
+                    String cpf = scanner.nextLine();
 
 
-        //Paciente 2
-        Paciente paciente2 = new Paciente();
-        paciente2.id = UUID.randomUUID();
-        paciente2.cpf = "111.111.111-12";
-        paciente2.nomeCompleto = "Lara Croft";
-        paciente2.dataNascimento = LocalDate.of(1991, 01, 01);
-        paciente2.endereco = "Rua 2";
-        paciente2.email = "email2@email.com";
-        paciente2.telefone = "(11)91111-1112";
-        System.out.println("Dados do paciente 2:" +
-                "\nNome: " + paciente2.nomeCompleto +
-                "\nID: " + paciente2.id +
-                "\nEmail: " + paciente2.email
-        );
+                    paciente.nomeCompleto = nome;
+                    paciente.email = email;
+                    paciente.cpf = cpf;
+                    paciente.id = UUID.randomUUID();
+                    break;
+                case 2:
+                    System.out.println("Dados do paciente:" +
+                            "\nNome: " + paciente.nomeCompleto +
+                            "\nEmail: " + paciente.email +
+                            "\nID: " + paciente.id +
+                            "\nCPF: " + paciente.cpf);
+                    break;
+                case 3:
+                    System.out.println("\n:::::Finalizando o sistema:::::");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
+        } while (opcao != 3);
+        scanner.close();
     }
 }
