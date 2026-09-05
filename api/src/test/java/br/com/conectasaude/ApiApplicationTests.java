f849f6614f4653c63c11e49d0c13d2e19a818cd5
marottomichael <michael_marotto@hotmail.com>
2026-09-04 23:15:09 -0300

    chore: configura estrutura inicial Spring Boot
    Adiciona a nova API Spring Boot com Java 21, Maven e Spring Web.
    Inclui endpoint GET /api/status para validação inicial do serviço.

---
+++ api/src/test/java/br/com/conectasaude/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.conectasaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
