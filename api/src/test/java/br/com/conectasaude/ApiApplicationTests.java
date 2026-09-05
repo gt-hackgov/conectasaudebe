ab37b82d27355e515793e87ef64a1fb7ba8bf53c
marottomichael <michael_marotto@hotmail.com>
2026-09-05 00:54:23 -0300

    test: isola configuração JWT dos testes

--- api/src/test/java/br/com/conectasaude/ApiApplicationTests.java
+++ api/src/test/java/br/com/conectasaude/ApiApplicationTests.java
@@ -3,7 +3,10 @@ package br.com.conectasaude;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "app.jwt.secret=MDEyMzQ1Njc4OWFiY2RlZjAxMjM0NTY3ODlhYmNkZWY=",
+        "app.jwt.expiration-minutes=60"
+})
 class ApiApplicationTests {
 
 	@Test
